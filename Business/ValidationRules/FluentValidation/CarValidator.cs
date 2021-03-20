dc35848858daa9df4dc08433afa5a8b065ba01aa
Emre Volkan UÇAR <emrevolkanucar@gmail.com>
2021-03-20 18:02:05 +0300

    Fluent Validation & AoP added.

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.ModelYear).NotEmpty();
+            RuleFor(c => c.ModelYear).GreaterThanOrEqualTo(1972);
+            RuleFor(c => c.DailyPrice).GreaterThan(100).When(c => c.BrandId == 1);
+            RuleFor(c => c.Description).Must(StartWithA).When(c => c.BrandId == 2);
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
